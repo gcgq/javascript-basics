8c7c0570a100380a9f0ba0840dac5a8997645497
gcgq <gary.quiso@gmail.com>
2017-07-09 22:02:34 -0500

    Added jquery stuff

--- js/script.js
+++ js/script.js
@@ -10,28 +10,27 @@ function addToList(li_label, li_child, li_content){
 
 function output_strings_init(fn_name){
   var strings ={
-    html:`<strong>${fn_name}</strong>:`,
+    html:`<strong>${fn_name}:</strong>`,
     console:`${fn_name}: `,
-    str: "",
-    li: document.createElement('li')
+    str: ""
   };
   return strings;
 }
 
 function compareInt() {
-    var num1 = parseInt( prompt("Enter a number"));
-    var num2 = parseInt( prompt("Enter another number"));
-    var output = output_strings_init("compareInt()");
+  var num1 = parseInt( prompt("Enter a number"));
+  var num2 = parseInt( prompt("Enter another number"));
+  var output = output_strings_init("compareInt()");
 
-    if(num1 > num2){
-      output.str += `${num1} is larger than ${num2}.`;
-    } else {
-      output.str += `${num2} is larger than ${num1}.`;
-    }
+  if(num1 > num2){
+    output.str += `${num1} is larger than ${num2}.`;
+  } else {
+    output.str += `${num2} is larger than ${num1}.`;
+  }
 
-    alert(output.console + output.str);
-    console.log(output.console + output.str);
-    addToList(output.html, 'p', output.str);
+  alert(output.console + output.str);
+  console.log(output.console + output.str);
+  addToList(output.html, 'p', output.str);
 }
 
 function sort1(){
@@ -86,7 +85,6 @@ function sort2(){
 
 function largest1(){
   var input_array = [];
-
   var prompt_str = "Enter a number";
   var output = output_strings_init("largest1()");
   output.str = "The largest number is: ";
@@ -106,40 +104,32 @@ function largest1(){
 function largest2(){
   var input_array = [];
   var largest_num = 0;
-  var fn_str = "largest2()";
-  var html_str = `<strong>${fn_str}</strong> `;
-  var console_str = `${fn_str} `;
   var prompt_str = "Enter a number";
-  var output_str = "The largest number is: ";
-
-  var li = answer_list.appendChild(document.createElement('li'));
+  var output = output_strings_init("largest2()");
+  output.str = "The largest number is: ";
 
   for(var i = 0; i < 5; i++){
     input_array[i] = parseInt( prompt(prompt_str + " (" + (i+1) + " of 5)") );
   }
+
   for(var i = 0; i < 5; i++){
     if(input_array[i] > largest_num){
       largest_num = input_array[i];
     }
   }
-
-  html_str += output_str + `${largest_num}`;
-  li.innerHTML = html_str;
-  alert(console_str + output_str + `${largest_num}`);
-  console.log(console_str + output_str + `${largest_num}`);
-  answer_list.insertBefore(li, answer_list.childNodes[0]);
+  output.str += `${largest_num}`;
+  alert(output.console + output.str);
+  console.log(output.console + output.str);
+  addToList(output.html, 'p', output.str);
+  // li.innerHTML = output.html;
+  // answer_list.insertBefore(li, answer_list.childNodes[0]);
 }
 
 function evenOdd() {
-  var fn_str = "evenOdd()";
-  var output_str = `<strong>${fn_str}</strong>: `;
-  // var li = answer_list.appendChild(document.createElement('li'));
-  var li = document.createElement('li');
-  li.innerHTML = output_str;
-  var ol = li.appendChild(document.createElement('ol'));
+  var output = output_strings_init("evenOdd()");
+  var ol = document.createElement('ol');
 
-  console.log(fn_str);
-
+  console.log(output.console);
   for(var i = 0; i <= 15; i++){
     if ((i % 2) == 0) {
       li_str = `${i} is even.`;
@@ -148,20 +138,16 @@ function evenOdd() {
     }
     console.log(li_str);
     ol.appendChild(document.createElement('li')).innerHTML = li_str;
-
   }
-  addToList(output_str, 'ol', ol.innerHTML);
+  addToList(output.html, 'ol', ol.innerHTML);
   // answer_list.insertBefore(li, answer_list.childNodes[0]);
 }
 
 function fizzbuzz(){
-  var fn_str = "fizzbuzz()";
-  var output_str = `<strong>${fn_str}</strong>: `;
-  var li = document.createElement('li');
-  li.innerHTML = output_str;
-  var ul = li.appendChild(document.createElement('ul'));
+  var output = output_strings_init("fizzbuzz()");
+  var ul = document.createElement('ul');
 
-  console.log(fn_str);
+  console.log(output.console);
   for(var i = 1; i <= 100; i++){
     var li_str = `${i}: `;
     if ((i % 3) == 0) {
@@ -173,17 +159,13 @@ function fizzbuzz(){
     console.log(`${i}: ${li_str}`);
     ul.appendChild(document.createElement('li')).innerHTML = li_str;
   }
-
   // answer_list.insertBefore(li, answer_list.childNodes[0]);
-  addToList(output_str, 'ul', ul.innerHTML);
+  addToList(output.html, 'ul', ul.innerHTML);
 }
 
 function multiplesOf3(){
-  var fn_str = "multiplesOf3()";
-  var output_str = `<strong>${fn_str}</strong>: `;
-  var li = answer_list.appendChild(document.createElement('li'));
-  li.innerHTML = output_str;
-  var ol = li.appendChild(document.createElement('ol'));
+  var output = output_strings_init("multiplesOf3()");
+  var ol = document.createElement('ol');
 
   var count = 0;
   for(var i = 1; i <= 1000; i++){
@@ -197,16 +179,12 @@ function multiplesOf3(){
   var total_str = `Final total: ${count}`;
   alert(total_str);
   console.log(total_str);
-  answer_list.insertBefore(li, answer_list.childNodes[0]);
+  addToList(output.html,'ol', ol.innerHTML);
 }
 
 function pyramid(){
-  var fn_str = "pyramid()";
-  var output_str = `<strong>${fn_str}</strong>: `;
-  var li = answer_list.appendChild(document.createElement('li'));
-  li.innerHTML = output_str;
-
-  var pre = li.appendChild(document.createElement('pre'));
+  var output = output_strings_init("pyramid()");
+  var pre = document.createElement('pre');
   var pre_line = "";
   var lines = [];
 
@@ -224,23 +202,21 @@ function pyramid(){
     }
     lines[j] = line_array.join("");
   }
-  console.log(fn_str);
+
+  console.log(output.console);
   for(var i=0; i < lines.length; i++){
     console.log(lines[i]);
     lines[i] += "&#10;"; //put a line break at the end of each line
     pre_line += lines[i];
   }
   pre.innerHTML = pre_line;
-  answer_list.insertBefore(li, answer_list.childNodes[0]);
+  addToList(output.html, 'pre', pre.innerHTML)
+  // answer_list.insertBefore(li, answer_list.childNodes[0]);
 }
 
 function diamond(){
-  var fn_str = "diamond()";
-  var output_str = `<strong>${fn_str}</strong>: `;
-  var li = answer_list.appendChild(document.createElement('li'));
-  li.innerHTML = output_str;
-
-  var pre = li.appendChild(document.createElement('pre'));
+  var output = output_strings_init("diamond()");
+  var pre = document.createElement('pre');
   var pre_line = "";
   var lines = [];
 
@@ -261,14 +237,15 @@ function diamond(){
     lines[j] = joined_line;
     lines[(number_array.length*2) - 2 - j] = joined_line;
   }
-  console.log(fn_str);
+
+  console.log(output.console);
   for(var i=0; i < lines.length; i++){
     console.log(lines[i]);
     lines[i] += "&#10;"; //put a line break at the end of each line
     pre_line += lines[i];
   }
   pre.innerHTML = pre_line;
-  answer_list.insertBefore(li, answer_list.childNodes[0]);
+  addToList(output.html, 'pre', pre.innerHTML);
 }
 
 function pyramid2(){
@@ -284,9 +261,11 @@ function pyramid2(){
   while( 0 >= range || range > 99){
     range = prompt("Enter a number between 1-99");
   }
+
   for (var i = 0; i < range; i++) {
     number_array[i] = i + 1;
   }
+
   for (var j = 0; j < number_array.length; j++) {
     line_array = [];
     for( var i = 0; i < number_array.length; i++){
@@ -309,8 +288,10 @@ function pyramid2(){
     var joined_line = line_array.join("");
     lines[j] = joined_line;
   }
+
   var pre_line = "";
   console.log(fn_str);
+
   for(var i=0; i < lines.length; i++){
     console.log(lines[i]);
     lines[i] += "&#10;" //put a line break at the end of each line
@@ -330,12 +311,15 @@ function diamond2(){
   var range = 0;
   var number_array = [];
   var lines = [];
+
   while( 0 >= range || range > 99){
     range = prompt("Enter a number between 1-99");
   }
+
   for (var i = 0; i < range; i++) {
     number_array[i] = i + 1;
   }
+
   for (var j = 0; j < number_array.length; j++) {
     line_array = [];
     for( var i = 0; i < number_array.length; i++){
@@ -363,8 +347,10 @@ function diamond2(){
     lines[j] = joined_line;
     lines[(number_array.length*2) - 2 - j] = joined_line;
   }
+
   var pre_line = "";
   console.log(fn_str);
+
   for(var i=0; i < lines.length; i++){
     console.log(lines[i]);
     lines[i] += "&#10;" //put a line break at the end of each line
@@ -377,53 +363,75 @@ function diamond2(){
 function todays_date(){
   today = new Date();
   date_str = `${('0'+today.getMonth()).slice(-2)}/${('0'+today.getDay()).slice(-2)}/${today.getFullYear()}`;
-  console.log(date_str);
+  var output = output_strings_init("todays_date()");
+
+  console.log(output.console);
+  addToList(output.html, 'p', date_str);
 }
 
 function leap_year_or_nah(year){
-  today = new Date();
   //year = parseInt(today.getFullYear());
+  var output = output_strings_init("leap_year_or_nah()")
   year = year;
-  output_str = `${year} is not a leap year`;
+  if(year == null){
+    year = prompt("Enter a year");
+  }
+  output.str = `${year} is not a leap year`;
+  console.log(output.console);
   if( year % 4 == 0){
     if(year % 100 == 0  && year % 400 != 0){
-      console.log(output_str);
+      console.log(output.str);
+      addToList(output.html, 'p', output.str);
       return false;
     } else {
-      output_str = `${year} is a leap year.`;
-      console.log(output_str);
+      output.str = `${year} is a leap year.`;
+      console.log(output.str);
+      addToList(output.html, 'p', output.str);
       return true;
     }
   } else {
-    console.log(output_str);
+    console.log(output.str);
+    addToList(output.html, 'p', output.str);
     return false;
   }
 }
 
 function consecutive_or_nah(input_ary){
-  input_qty = 5;
-  number_array = input_ary;
-  var nah = "not consecutive";
-  if (number_array == null){
+  var output = output_strings_init("consecutive_or_nah()");
+  var input_qty = 5;
+  var number_ary = input_ary;
+  var consecutive_str = "is not consecutive.";
+
+  if (number_ary == null){
+    number_ary = [];
     for(var i = 0; i < input_qty; i++) {
-    number_array[i] = parseInt(prompt(`Enter a number (${i+1} of ${input_qty})`));
+      number_ary[i] = parseInt(prompt(`Enter a number (${i+1} of ${input_qty})`));
     }
   }
-  for (var i = 0; i < number_array.length -1 ; i++) {
-    if (number_array[i] + 1 != number_array[i+1]) {
-      console.log(nah);
+
+  for (var i = 0; i < number_ary.length -1 ; i++) {
+    if (number_ary[i] + 1 != number_ary[i+1]) {
+      output.str = `${number_ary} ${consecutive_str}`;
+      console.log(output.console + output.str);
+      addToList(output.html, 'p', output.str);
       return false;
     }
   }
-  console.log("Consecutive.");
+
+  consecutive_str = "is consecutive.";
+  output.str = `${number_ary} ${consecutive_str}`;
+  console.log(output.console + output.str);
+  addToList(output.html, 'p', output.str);
   return true;
 }
 
-function repeats_or_nah1(){
+function repeats_or_nah(){
+  var output = output_strings_init("repeats_or_nah()");
   // var input_qty = 5;
   var number_array = [1,2,1,4,5,6,3,8,3,5,6,3,5,6,7];
   var output_str = "";
   var matches={'keys':[]};
+
   for (var i = 0; i < number_array.length; i++) {
     if(matches[number_array[i]] == null){
         matches[number_array[i]] = 1;
@@ -432,17 +440,19 @@ function repeats_or_nah1(){
       matches[number_array[i]] += 1;
     }
   }
+
   for(i = 0; i < matches['keys'].length; i++){
     if( matches[matches['keys'][i]] > 1 ){
       console.log(`${matches['keys'][i]} has ${matches[matches['keys'][i]]} repeats`);
     }
   }
-
+  addToList(output.str, 'ul', ul.innerHTML);
   console.log(matches);
   return matches;
 }
 
 function check_multiple(array){
+  var output = output_strings_init("check_multiple()");
   array.sort();
   for (var i = 0; i < array.length-1; i++) {
     if( array[i] === array[i+1] ){
@@ -453,20 +463,39 @@ function check_multiple(array){
 }
 
 function first_element(input_ary, n){
-  if( n == null){
+  var output = output_strings_init("first_element()");
+  if(input_ary == null){
+    input = prompt("Enter an array of numbers (separate values with commas)");
+    n = prompt("Enter number of elements to return");
+    input_ary = JSON.parse("[" + input + "]");
+  }
+
+  if( n == null || n == ""){
+    output.str += input_ary[0];
+    addToList(output.html, 'p', output.str);
     return input_ary[0];
   } else {
+    output.str += input_ary.slice(0,n);
+    addToList(output.html, 'p', output.str);
     return input_ary.slice(0,n);
   }
 }
 
 function join_elements(){
-  var color_ary = ["Red", "Green", "White", "Black"];
-  var output_str = `"${color_ary.join(' ')}" OR "${color_ary.join(',')}" OR "${color_ary.join('+')}"`;
-  console.log(output_str);
+  var input = input;
+  var output = output_strings_init("join_elements()");
+  var ary = ["Red", "Green", "White", "Black"];
+  if(input == null){
+    input = prompt("Enter strings to join (separate items with commas. put strings in quotes)");
+    ary = JSON.parse("["+ input +"]");
+  }
+  output.str = `"${ary.join(" ")}" OR "${ary.join(',')}" OR "${ary.join('+')}"`;
+  addToList( output.html, 'p', output.str);
+  console.log(output.console + output.str);
 }
 
 function frequency(){
+  var output = output_strings_init("frequency()");
   var input_ary = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
   var counts = {'keys':[]};
   var freq = 1;
@@ -494,6 +523,7 @@ function frequency(){
 }
 
 function ordinals(){
+  var output = output_strings_init("ordinals()");
   var color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
   var o = ["st","nd","rd", "th"];
   for (var i = 0; i < color.length; i++) {
@@ -506,6 +536,7 @@ function ordinals(){
 }
 
 function find_duplicates(input_ary){
+  var output = output_strings_init("find_duplicates()");
   for (var i = 0; i < input_ary.length; i++) {
     for (var j = i+1; j < input_ary.length; j++) {
       if (input_ary[i] == input_ary[j]) {
@@ -516,6 +547,7 @@ function find_duplicates(input_ary){
 }
 
 function is_palindrome(input_str){
+  var output = output_strings_init("is_palindrome()");
   for(var i=0; i< input_str.length / 2; i++){
     if(input_str.charAt(i) != input_str.charAt(input_str.length-1-i)){
       return false;
@@ -525,6 +557,7 @@ function is_palindrome(input_str){
 }
 
 function reverse_string(input_str){
+  var output = output_strings_init("reverse_string()");
   output_str = "";
   for(i=input_str.length; i >=0; i--){
     output_str += input_str.charAt(i);
@@ -534,6 +567,7 @@ function reverse_string(input_str){
 }
 
 function string_slicer(input_str){
+  var output = output_strings_init("string_slicer()");
   output_ary =[];
   for (var i = 0; i <= input_str.length; i++) {
     for (var j = i+1; j <= input_str.length; j++) {
@@ -544,18 +578,21 @@ function string_slicer(input_str){
 }
 
 function alphabetize(input_str){
+  var output = output_strings_init("alphabetize()");
   return input_str.split("").sort().join("");
 }
 
 function second_bananas(input_array){
+  var output = output_strings_init("second_bananas()");
   input_array.sort(function (a, b) { return a - b; });
   return [input_array[1], input_array[input_array.length-2]];
 }
 
 function remove_dulpicate_letters(input_str){
+  var output = output_strings_init("remove_dulpicate_letters()");
   output_str = input_str.split("");
   for (var i = 0; i < output_str.length; i++) {
-    for (var j = i+1; j < output_str.length; j++) {
+    for (var j = i+1; j < output_str.length-1; j++) {
       if(output_str[i] == output_str[j]){
         output_str[j] = null;
       }
@@ -564,16 +601,86 @@ function remove_dulpicate_letters(input_str){
   return output_str.join("");
 }
 
-// compareInt();
-// sort1();
-// sort2();
-// largest1();
-// largest2();
-// evenOdd();
-// fizzbuzz();
-// multiplesOf3();
-// pyramid();
-// diamond();
-// todays_date()
-// leap_year_or_nah();
-// consecutive_or_nah();
+function js_style(){
+  var paragraph = document.getElementById("change-this-text");
+  paragraph.innerHTML = "<h1>Boom.</h1> <h6>(changed the background color, too.)</h6>";
+  paragraph.style.color = "red";
+  paragraph.style.background = "black" ;
+  paragraph.style.fontSize="300%";
+  document.getElementById("jsstyle").style.display="none";
+}
+
+function getFormValue(){
+  // var input = document.getElementById("form1").submit();
+  // alert(input);
+  var first_name = document.querySelector("[name='fn']").value;
+  var last_name = document.querySelector("[name='ln']").value;
+  alert(`Hi, ${first_name} ${last_name}`);
+}
+
+function highlight(){
+  var bs = document.getElementsByTagName('b');
+  for(var i=0; i< bs.length; i++){
+    bs[i].style.background = "yellow";
+  }
+}
+
+function lowlight(){
+  var bs = document.getElementsByTagName('b');
+  for(var i=0; i< bs.length; i++){
+    bs[i].style.background = "";
+  }
+}
+
+function getAttributes(){
+  var link = document.getElementById('w3r');
+  var output_str = "DOM version\n\n";
+  for( var i=0; i < link.attributes.length; i++){
+    output_str += `${link.attributes[i].name} = ${link.attributes[i].value}\n`;
+  }
+  alert(output_str);
+}
+
+$(document).ready(function(){
+  $("#jsstyle2").click(function(){
+    var $p = $("#change-this-text");
+    $p.html("<h1>Boom.</h1> <h6>(changed the background color, too.)</h6>");
+    $p.css({"color": "yellowgreen", "background": "blue", "font-size": "300%"});
+    $("#jsstyle2").css({"display": "none"});
+  });
+
+  $("#form2").submit(function (event){
+    var first_name = $(this).find("input[name='fn']").val();
+    var last_name = $(this).find("input[name='ln']").val();
+    alert(`Hi, ${first_name} ${last_name}`);
+    event.preventDefault();
+  });
+
+  $("#highlightstuff").mouseenter(function(){
+    $('b').css("background","yellow")
+  });
+
+  $("#highlightstuff").mouseleave(function(){
+    $('b').css("background","");
+  });
+
+  $("#attribute-button").click(function(){
+    var attr = $("#w3r")[0].attributes;
+    var output_str = "jQuery version\n\n";
+    for (var i = 0; i < attr.length; i++) {
+      output_str += `${attr[i].name} = ${attr[i].value}\n`;
+    }
+    alert(output_str);
+  });
+});
+
+
+// function lowlightJQ(){
+//
+// }
+// function highlightJQ(){
+//
+// }
+// function getAttributesJQ(){
+//
+// }
